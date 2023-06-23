ed8b622770c77e7fa59fdbf3cac105b7baeae35f
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-06-23 13:59:02 +0200

    Test 6 in progress

--- src/validBraces.js
+++ src/validBraces.js
@@ -4,20 +4,27 @@ function validBraces(str) {
     return false;
   }
 
-  leftArray = { 
-    ")": "(",
-    "]": "[",
-    "}": "{",
+  let pairOfBrackets = { 
+    ')': '(',
+    ']': '[',
+    '}': '{',
   };
 
-
+  console.log('str: ', str);
+  let arrLeftBrackets = [];
   for (i = 0; i < str.length; i++){
-    let char = str[i]
-    if (Object.keys(leftArray).includes(char)){
-      
+    let char = str[i];
+    if (Object.values(pairOfBrackets).includes(char)){
+      console.log('leftBrackets includes this char: ', char);
+      arrLeftBrackets.push(char);
+    } else {
+      if(char === arrLeftBrackets[arrLeftBrackets.length-1]){
+        arrLeftBrackets.pop();
+      }
     }
+    console.log('arrLeftBrackets: ', arrLeftBrackets);
   }
-
+  
   return true;
 }
 
