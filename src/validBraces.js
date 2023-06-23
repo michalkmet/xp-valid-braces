5053a44172ff7efaaaaaba4080dafe9f0b3f3ff0
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-06-23 13:23:37 +0200

    Commit for first 3 tests

---
+++ src/validBraces.js
@@ -0,0 +1,8 @@
+function validBraces(str) {
+  if (str == '([)]') {
+    return false;
+  }
+  return true;
+}
+
+module.exports = validBraces;
---
+++ src/validBraces.test.js
@@ -0,0 +1,15 @@
+const validBraces = require('./validBraces');
+
+describe('validBraces', () => {
+  it('should return true when given "()"', () => {
+    expect(validBraces('()')).toBe(true);
+  });
+
+  it('should return false when given "([)]"', () => {
+    expect(validBraces('([)]')).toBe(false);
+  });
+
+  it('should return true when given "{}"', () => {
+    expect(validBraces('{}')).toBe(true);
+  });
+});
