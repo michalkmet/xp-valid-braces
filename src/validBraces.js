699bcc09e3d2e07f21dbe9672f3c0699969c77fa
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-06-23 14:22:25 +0200

    1th test pass again

--- src/validBraces.js
+++ src/validBraces.js
@@ -1,25 +1,20 @@
 function validBraces(str) {
-  if (str.length % 2 != 0) return false;
-  if (['([)]', '[{]}'].includes(str)) {
-    return false;
-  }
-
   const pairOfBrackets = {
-    ')': '(',
-    ']': '[',
-    '}': '{',
+    '(': ')',
+    '[': ']',
+    '{': '}',
   };
 
   console.log('str: ', str);
   let arrLeftBrackets = [];
   for (i = 0; i < str.length; i++){
     let char = str[i];
-    if (Object.values(pairOfBrackets).includes(char)){
-      console.log('leftBrackets includes this char: ', char);
+    if (Object.keys(pairOfBrackets).includes(char)){
+      console.log('pairOfBrackets includes this char: ', char);
       arrLeftBrackets.push(char);
     } else {
-      console.log(char);
-      console.log('pair: ', pairOfBrackets[arrLeftBrackets.pop()]);
+      console.log('char: ', char);
+      // console.log('pair: ', pairOfBrackets[arrLeftBrackets.pop()]);
       if (char !== pairOfBrackets[arrLeftBrackets.pop()]) {
         console.log(char);
         return false;
@@ -27,7 +22,7 @@ function validBraces(str) {
     }
     console.log('arrLeftBrackets: ', arrLeftBrackets);
   }
-  
+
   return true;
 }
 
